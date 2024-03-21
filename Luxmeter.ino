241f4d2306b78dc0e1ea075256dfc1a8dda89b57
hyperpixel34 <80213902+hyperpixel34@users.noreply.github.com>
2024-03-21 21:48:17 +0100

    Finales Programm

--- Luxmeter.ino
+++ Luxmeter.ino
@@ -1,60 +1,238 @@
-#include <LiquidCrystal_I2C.h>//monitor 
 #include <LiquidCrystal.h>
 #include <Wire.h>
 
-#define RESISTOR 9800.0
-#define SENSOR A2
+#define RESISTOR 4700.0
+#define SENSOR A1
+
+#define RIGHT 0
+#define UP 1
+#define DOWN 2
+#define LEFT 3
+#define SELECT 4
+#define NONE 5
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
+long cycles = 0;
+
+float UIN = 0.0;
+float UOUT = 0.0;
+float RESISTANCE = 0.0;
+float LUX = 0.0;
+float MAX = 0.0;
+float MIN = 0.0;
+long nextPrint;
+
+int DIV = 1;
+const float DIVISORS[5] = {2.0, 4.0, 8.0, 16.0, 32.0};
+
+int Mode = 0; // 0: Standard; 1: Min/Max; 2: Divisoreinstellung
+
+byte omega[] = {
+  B00000,
+  B01110,
+  B10001,
+  B10001,
+  B10001,
+  B01010,
+  B11011,
+  B00000
+};
+
+byte ArrowChar[] = {
+  B00100,
+  B01110,
+  B10101,
+  B00100,
+  B00100,
+  B00100,
+  B00100,
+  B00000
+};
+
+void Measure()
+{
+  UIN = analogRead(SENSOR);
+  UOUT = UOUT + (UIN - UOUT) / DIVISORS[DIV]; // Tiefpass
+  RESISTANCE = Resistance(Voltage(UOUT));
+  LUX = f(RESISTANCE);
+
+  // Maximums- und Minumumswerte schreiben, wenn mehr als 100 Cycles durchgegangen sind
+  if (cycles > 100)
+  {
+    if (LUX > MAX) { MAX = LUX; }
+    else if (LUX < MIN) { MIN = LUX; }
+  }
+  
+  else
+  {
+    MIN = LUX;
+  }
+
+  cycles++;
+}
+
+void StandardMode()
+{
+  lcd.setCursor(0,0);
+  lcd.print("Lux: "+(String)(LUX) + " lx    ");
+  
+  lcd.setCursor(0,1);
+  lcd.print("R: "+(String)(RESISTANCE));
+  
+  lcd.createChar(1, omega);
+  lcd.setCursor(11,1);
+  lcd.write(1);
+}
+
+void MinMax()
+{
+  lcd.setCursor(0,0);
+  lcd.print("Max: "+(String)(MAX) + " lx    ");
+  
+  lcd.setCursor(0,1);
+  lcd.print("Min: "+(String)(MIN) + " lx    ");
+}
+
+void SelectDivisor()
+{
+  lcd.setCursor(2,0);
+  lcd.print("2 4 8 16 32");
+
+  switch (Buttons())
+  {
+    case LEFT:
+    {
+      int currentArrowPos = ArrowPosition(DIV);
+      PreviousDivisor();
+      while (Buttons() == LEFT) {}
+      lcd.setCursor(currentArrowPos, 1);
+      lcd.print(" ");
+      break;
+    }
+
+    case RIGHT:
+    {
+      int currentArrowPos = ArrowPosition(DIV);
+      NextDivisor();
+      while (Buttons() == RIGHT) {}
+      lcd.setCursor(currentArrowPos, 1);
+      lcd.print(" ");
+      break;
+    }
+  }
+  
+  lcd.createChar(1, ArrowChar);
+  lcd.setCursor(ArrowPosition(DIV), 1);
+  lcd.write(1);
+  
+}
+
+int ArrowPosition(int index)
+{
+  switch (index)
+  {
+    case 0:
+    {
+      return 2;
+      break;
+    }
+    
+    case 1:
+    {
+      return 4;
+      break;
+    }
+    
+    case 2:
+    {
+      return 6;
+      break;
+    }
+
+    case 3:
+    {
+      return 8;
+      break;
+    }
+
+    case 4:
+    {
+      return 11;
+      break;
+    }
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(A1, INPUT);
-  Serial.begin(9600);
+  pinMode(SENSOR, INPUT);
+  pinMode(A0, INPUT);
   lcd.begin(16, 2);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  lcd.setCursor(0,0);
-  int value = analogRead(SENSOR);
-  float resistance = Resistance(Voltage(value));
+  nextPrint = millis() + 100;
 
-  // Tiefpass
-  int temp = 0;
-
-  for (int i = 0; i<50; i++)
+  // Taster abfragen
+  
+  switch (Buttons())
   {
-    temp += Measure();
-    delay(10);
+    case UP:
+    {
+      PreviousMode();
+      while (Buttons() == UP) {}
+      lcd.clear();
+      break;
+    }
+
+    case DOWN:
+    {
+      NextMode();
+      while (Buttons() == DOWN) {}
+      lcd.clear();
+      break;
+    }
+  }
+
+
+  switch (Mode)
+  {
+    case 0:
+    {
+      StandardMode();
+      break;
+    }
+
+    case 1:
+    {
+      MinMax();
+      break;
+    }
+
+    case 2:
+    {
+      SelectDivisor();
+      break;
+    }
   }
   
-  Serial.println(Measure());
-  lcd.print("Lux: "+(String)(temp/50.0));
-  
-  lcd.setCursor(0,1);
-  lcd.print("R: "+(String)resistance+" Ohm");
-  delay(500);
+  // Werte auslesen
+  while (millis() < nextPrint)
+  {
+    Measure();
+  }
 }
 
-float Measure()
+int Filter(int value)
 {
-  int value = analogRead(SENSOR);
-  float resistance = Resistance(Voltage(value));
-  
-  if (resistance < 419.0)
-  {
-    return f1(resistance);
-  }
-  else
-  {
-    return f2(resistance);
-  }
+  return UOUT + (UIN - UOUT) / DIV;
 }
 
 float Resistance(float voltage)
 {
-  return (5.0*RESISTOR/voltage)-RESISTOR;
+  return (5.0*RESISTOR/(5.0-voltage))-RESISTOR;
 }
 
 float Voltage(int value)
@@ -62,12 +240,72 @@ float Voltage(int value)
   return value/1023.0*5.0;
 }
 
-float f1(float x)
+float f(float x)
 {
-  return pow(x/56889.5860605443, 1.0/-0.732075748353836);
+  return 2.0 * pow((x-70.0)/33993.95062, 1.0/-0.6535);
 }
 
-float f2(float x)
+void NextMode()
 {
-  return pow(x/11864.12303184592,1.0/-0.478173839323303);
+  if (Mode == 2)
+  {
+    Mode = 0;
+  }
+  else
+  {
+    Mode++;
+  }
+}
+
+void PreviousMode()
+{
+  if (Mode == 0)
+  {
+    Mode = 2;
+  }
+  
+  else
+  {
+    Mode--;
+  }
+}
+
+void NextDivisor()
+{
+  if (DIV == 4)
+  {
+    DIV = 0;
+  }
+  else
+  {
+    DIV++;
+  }
+}
+
+void PreviousDivisor()
+{
+  if (DIV == 0)
+  {
+    DIV = 4;
+  }
+  
+  else
+  {
+    DIV--;
+  }
+}
+
+
+int Buttons()
+{
+  int value = analogRead(A0); // Auslesen der Taster am Analogen Pin A0.
+  if (value > 1000) return NONE;
+  if (value < 50) return RIGHT;
+  if (value < 195) return UP;
+  if (value < 380) return DOWN;
+  if (value < 555) return LEFT;
+  if (value < 680) return SELECT;
+  if (value < 790) return RIGHT;
+   
+  return NONE; // Ausgabe wenn kein Taster gedrückt wurde.
 }
